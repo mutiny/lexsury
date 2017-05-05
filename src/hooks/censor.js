1635dfa309473597e3756e0d02b3db68401e228a
Robin Ury <r.ury@tuta.io>
2017-05-05 16:13:17 -0500

    Refactored type handling of JSON post type to utilize built in hook

--- src/hooks/censor.js
+++ src/hooks/censor.js
@@ -1,13 +1,15 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+const commonHooks = require('feathers-hooks-common')
 const Filter = require('bad-words')
+
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function (hook) {
-    hook.data = [].concat(hook.data).map(obj => {
-      let filter = new Filter()
-      obj.text = filter.clean(obj.text)
-      return obj
-    })
-    return Promise.resolve(hook)
+    let questions = commonHooks.getItems(hook)
+    let filter = new Filter()
+    if (!Array.isArray(questions)) {
+      questions.text = filter.clean(questions.text)
+    } else { questions.forEach((item, idx) => { questions[idx].text = filter.clean(item.text) }) }
+    commonHooks.replaceItems(hook, questions)
   }
 }
