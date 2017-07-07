ca4e90ce911f6444189176ef7dcf1c3cd230c1d0
Bin Ury <bin.ury@tuta.io>
2017-07-07 12:55:20 -0500

    Add creatorId association creation-hook to room service

--- src/services/room/room.hooks.js
+++ src/services/room/room.hooks.js
@@ -1,12 +1,16 @@
 const { authenticate } = require('feathers-authentication').hooks;
 const genRandomName = require('../../hooks/gen-rand-room-name.js');
+const hooks = require('feathers-authentication-hooks');
 
 module.exports = {
   before: {
     all: [ authenticate('jwt') ],
     find: [],
     get: [],
-    create: [ genRandomName() ],
+    create: [
+      genRandomName(),
+      hooks.associateCurrentUser({ idField: 'id', as: 'creatorId' }),
+    ],
     update: [],
     patch: [],
     remove: []
